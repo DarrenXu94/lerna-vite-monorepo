0032dcb7c06af9495f08e567580aa053bda093c1
Darren Xu <darrenxu1994@gmail.com>
2025-11-08 16:47:27 +1100

    Added header stories

--- packages/ds-common/src/components/DsButton/DsButton.stories.ts
+++ packages/ds-common/src/components/DsButton/DsButton.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
-import Button from "./MyButton.vue";
+import Button from "./DsButton.vue";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
